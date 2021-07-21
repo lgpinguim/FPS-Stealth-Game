143f73419a0dfd64bbb68cd85ff4984b75cbcd07
Luis Ferreira <lg_pinguim@hotmail.com>
2021-07-20 22:55:59 -0300

    Made improvements to the extraction zone.

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -3,6 +3,7 @@
 
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
 
 
 // Sets default values
@@ -21,6 +22,11 @@ AFPSExtractionZone::AFPSExtractionZone()
 	OverlapComp->SetHiddenInGame(false);
 
 	OverlapComp->OnComponentBeginOverlap.AddDynamic(this,&AFPSExtractionZone::HandleOverlap);
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp-> DecalSize = FVector(200.0f,200.0f,200.0f);
+	DecalComp->SetupAttachment(RootComponent);
+	
 }
 
 
